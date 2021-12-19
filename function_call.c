a3fd72434bb3dd2da60494085e89752667ab663b
Durga Prasad VIyyapu <39397307+viyyamprasadu@users.noreply.github.com>
2021-12-19 11:46:56 +0530

    Create function_call.c
    do the math and display LinkedList

---
+++ function_call.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "function_call.h"
+
+int do_math(int x, int y, char ch)
+{
+    if(ch == '+') return x+y;
+    if(ch == '-') return x-y;
+    if(ch == '*') return x*y;
+    if(ch == '/') return x/y;
+}
+
+void display_List(struct Node *head,int n)
+{
+    static int i=0;
+	printf("display the list : %d\n",head->data);
+	n--;
+	i++;
+	if(n >0)
+		display_List(head+i,n);
+}
